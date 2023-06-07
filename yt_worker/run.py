af217f550a09fb792f1a212a922f14338248e526
Olga <murmurshik3@yandex.ru>
2023-06-07 18:53:10 +0700

    more work

---
+++ yt_worker/run.py
@@ -0,0 +1,4 @@
+# Create Worker object and make all prerequerments
+
+def start_worker():
+    return NotImplementedError
---
+++ yt_worker/worker.py
@@ -0,0 +1,12 @@
+
+class Worker:
+    """
+    Class for accepting tasks and running them. Conteins all async logic.
+    All methods with _async suffix are async.
+    """
+    def __init__(self) -> None:
+        pass
+        
+    def add_task(self) -> None:
+        pass
+
